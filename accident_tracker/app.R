2f7024d6c8c8d3b515b30669b27dc2f7483593c0
Abigail Vickers <ahopevickers@gmail.com>
2021-11-02 15:13:22 -0400

    Created blank shiny app

--- accident_tracker/app.R
+++ accident_tracker/app.R
@@ -1,1 +1,17 @@
 library(shiny)
+
+# Define UI
+
+ui <- fluidPage(
+  
+)
+
+# Define server logic
+
+server <- function(input, output) {
+  
+}
+
+# Run the app 
+
+shinyApp(ui = ui, server = server)
