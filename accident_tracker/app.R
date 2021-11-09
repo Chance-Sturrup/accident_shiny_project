5739312c60395389d1c0aad7dbc51c5bfca4f132
Abigail Vickers <ahopevickers@gmail.com>
2021-11-09 14:48:17 -0500

    Changing name of data frame to align with class style guide

--- accident_tracker/app.R
+++ accident_tracker/app.R
@@ -2,7 +2,7 @@ library(shiny)
 library(leaflet)
 library(tidyverse)
 
-US_Accidents <- read_csv("../data/US_Accidents2019.csv")
+us_accidents <- read_csv("../data/US_Accidents2019.csv")
 
 source("R/ui.R")
 
