8c8ec593c762eba9cc9cf6080c1f5c61241089b4
bestrem <89321623+bestrem@users.noreply.github.com>
2021-11-12 12:44:30 -0500

    Added color functionality

--- accident_tracker/R/ui.R
+++ accident_tracker/R/ui.R
@@ -1,7 +1,17 @@
+
+#colorSelect Function
+#Creates UI selecter to choos how the data points on the map are colored 
+colorSelect <- function() {
+  selectInput("color", label = "Color the Data by:",
+              c("None", "Severity", "Temperature (F)"))
+}
+  
 ui <- function() {
   fluidPage(titlePanel("US Car Accidents in 2019"),
             mainPanel(#this will create a space for us to display our map
               leafletOutput(outputId = "mymap"),
+              #Execute colorSelect function to make UI selector
+              colorSelect()
             ))
   
 }
