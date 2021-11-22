c348197e5f32f595f3790c71081dc42547d18e3b
bestrem <89321623+bestrem@users.noreply.github.com>
2021-11-22 18:33:25 -0500

    Added ability to color by Day/Night - BE

--- accident_tracker/app.R
+++ accident_tracker/app.R
@@ -7,7 +7,7 @@ source("DataOrganize.R")
 #Creates UI selecter to choos how the data points on the map are colored 
 colorSelect <- function() {
   selectInput("color", label = "Color the Data by:",
-              c("None", "Severity", "Temperature (F)", "Precipitation", "Visibility"))
+              c("None", "Severity", "Temperature (F)", "Precipitation", "Visibility", "Day/Night"))
 }
 #Filtering function
 #Filter and reorganize the input options
@@ -28,8 +28,8 @@ filterweather <- function() {
   )}
 filtermonth <- function() {                     
   selectInput(inputId = "Month", label = "Month:",
-                     choices =c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),
-                     multiple = TRUE
+              choices =c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),
+              multiple = TRUE
   )}
 
 filtertime <- function() {     
@@ -60,19 +60,19 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
- output$mymap <- renderLeaflet({
-   if (input$sunrise != "All") {
-     us_accidents <- filter(us_accidents, us_accidents$day.night ==input$sunrise)
-   }
-   if (is.null(input$weather) == FALSE) {
-     us_accidents <- filter(us_accidents, us_accidents$Weather %in% input$weather)
-   }
-   if (is.null(input$Month) == FALSE) {
-     us_accidents <- filter(us_accidents, us_accidents$month %in% input$Month)
-   }
-   if (is.null(input$actualtime) == FALSE) {
-     us_accidents <- filter(us_accidents, us_accidents$hour %in% input$actualtime)
-   }
+  output$mymap <- renderLeaflet({
+    if (input$sunrise != "All") {
+      us_accidents <- filter(us_accidents, us_accidents$day.night ==input$sunrise)
+    }
+    if (is.null(input$weather) == FALSE) {
+      us_accidents <- filter(us_accidents, us_accidents$Weather %in% input$weather)
+    }
+    if (is.null(input$Month) == FALSE) {
+      us_accidents <- filter(us_accidents, us_accidents$month %in% input$Month)
+    }
+    if (is.null(input$actualtime) == FALSE) {
+      us_accidents <- filter(us_accidents, us_accidents$hour %in% input$actualtime)
+    }
     if (input$color == "None") {
       selectedColor <- "Black"
       colorPal <- colorBin(selectedColor, domain = NULL)
@@ -89,41 +89,39 @@ server <- function(input, output, session) {
       colorData = us_accidents$temp
       legend = "Temperature (F)"
     }
-   if (input$color == "Precipitation") {
-     colorPal <- colorBin("BrBG", domain = us_accidents$precip)
-     colorData <- us_accidents%>%precip
-     legend = "Precipitation"
-   }
-#   if (input$color == "Day/Night") {
-#     colorPal <- colorBin("OrRd", domain = us_accidents$day.night)
-#     colorData <- us_accidents %>%day.night
-#     legend = "Day/Night"
-#   }
-   if (input$color == "Visibility") {
-     colorPal <- colorBin("YlGnBu", domain = us_accidents$vis)
-     colorData = us_accidents$vis
-     legend = "Visibility"
-   }
-   isolate({
-     if ("mymap_center" %in% names(input)) {
-       mapparams <- list(center = input$mymap_center,
-                         zoom = input$mymap_zoom)
-     } else {
-       mapparams <- list(center = list(lng=-86.5804, lat=35.5175),
-                         zoom = 7)
-     }
-   })
-   
+    if (input$color == "Precipitation") {
+      colorPal <- colorBin("BrBG", domain = us_accidents$precip)
+      colorData <- us_accidents$precip
+      legend = "Precipitation"
+    }
+    if (input$color == "Day/Night") {
+      colorPal <- colorFactor("Dark2", domain = us_accidents$day.night)
+      colorData <- us_accidents$day.night
+      legend = "Day/Night"
+    }
+    if (input$color == "Visibility") {
+      colorPal <- colorBin("YlGnBu", domain = us_accidents$vis)
+      colorData = us_accidents$vis
+      legend = "Visibility"
+    }
+    isolate({
+      if ("mymap_center" %in% names(input)) {
+        mapparams <- list(center = input$mymap_center,
+                          zoom = input$mymap_zoom)
+      } else {
+        mapparams <- list(center = list(lng=-86.5804, lat=35.5175), zoom = 7)
+      }
+    })
+    
     #colorPal <- colorBin(selectedColor, domain = data)
-   labels <- sprintf(
-     "<strong>%s</strong>%s%s%s%s%s<br/><strong>%s</strong>%s<br/><strong>%s</strong>%s",
-     "Place of accident: ", us_accidents$city," ", us_accidents$state,", ", us_accidents$zip,
-     "Time of accident: ",us_accidents$time,
-     "Weather: ",us_accidents$wthr.cond
-   ) %>% lapply(htmltools::HTML)
-   
-    leaflet(data = us_accidents,
-            options = leafletOptions(minZoom = 4, maxZoom = 20)) %>%
+    labels <- sprintf(
+      "<strong>%s</strong>%s%s%s%s%s<br/><strong>%s</strong>%s<br/><strong>%s</strong>%s",
+      "Place of accident: ", us_accidents$city," ", us_accidents$state,", ", us_accidents$zip,
+      "Time of accident: ",us_accidents$time,
+      "Weather: ",us_accidents$wthr.cond
+    ) %>% lapply(htmltools::HTML)
+    
+    leaflet(options = leafletOptions(minZoom = 4, maxZoom = 20)) %>%
       setView(lng = mapparams$center$lng, lat = mapparams$center$lat, zoom = mapparams$zoom) %>%
       addTiles() %>%
       addCircleMarkers(lng = ~ lng, lat = ~ lat, radius = 5,
@@ -134,12 +132,14 @@ server <- function(input, output, session) {
                                                              # at zoom level 14
                                                              spiderfyOnMaxZoom = FALSE),
                        label =labels,
-                       labelOptions = labelOptions(
-                         style = list("font-weight" = "normal", padding = "3px 8px"),
-                         textsize = "11px",
-                         direction = "auto")
+                       labelOptions = labelOptions(style = list("font-weight" = "normal", 
+                                                                padding = "3px 8px"),
+                                                   textsize = "11px",
+                                                   direction = "auto")
       ) %>%
-      addLegend("bottomright", pal = colorPal, values = data,
+      addLegend("bottomright", 
+                pal = colorPal, 
+                values = colorData,
                 title = legend,
                 opacity = 1)
   })
