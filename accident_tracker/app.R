0af6ccbfce19ca7c8e2e907bb05745aad32c230e
Abigail Vickers <ahopevickers@gmail.com>
2021-12-02 10:52:17 -0500

    Changed color_data to give NULL when "None" is chosen and changed breaks for precipitation color bin.

--- accident_tracker/app.R
+++ accident_tracker/app.R
@@ -89,7 +89,7 @@ color_pal <- function(a){
          "None" =  colorBin("black", domain = NULL),
          "Severity" = colorFactor("YlOrRd", domain = us_accidents$severity),
          "Temperature (F)" = colorBin("RdBu", reverse = TRUE, domain = us_accidents$temp, bins = c(-50, 0, 32, 50, 80, 100, 175)),
-         "Precipitation" = colorBin("BrBG", domain = us_accidents$precip, bins = c(0, 0.001, 0.05, 0.1, 0.3, 1, 25)),
+         "Precipitation" = colorBin("BrBG", domain = us_accidents$precip, bins = c(0, 0.001, 0.05, 0.1, 0.3, 1, 2.0)),
          "Day/Night" = colorFactor("Dark2", domain = us_accidents$day.night),
          "Visibility" = colorBin("YlGnBu", domain = us_accidents$vis, bins = c(0, 0.5, 2, 150))
   )
@@ -97,7 +97,7 @@ color_pal <- function(a){
 
 color_data <- function(b){
   switch(b,
-         "None" = us_accidents$severity,
+         "None" = NULL,
          "Severity" = us_accidents$severity,
          "Temperature (F)" = us_accidents$temp,
          "Precipitation" = us_accidents$precip,
@@ -127,6 +127,16 @@ server <- function(input, output, session) {
       mapparams <- list(center = list(lng=-86.5804, lat=35.5175), zoom = 7)
     }
   })
+  
+  # pal <- reactive ({
+  #   color_pal(input$color)
+  #   colorData <- color_data(input$color)
+  # })
+  # 
+  # c_data <- reactive ({
+  #   s <-color_data(input$color)
+  #   s
+  # })
   
   output$mymap <- renderLeaflet({
     if (input$sunrise != "All") {
